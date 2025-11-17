b8dd2cfcae8e9896489f87fe4e69e82c402158eb
josemorista <josemorista@gmail.com>
2025-11-17 20:14:42 -0300

    ci: clojars deploy

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject eboshi "0.1.0"
+(defproject org.clojars.josemorista/eboshi "0.1.0"
   :description "Eboshi is a lightweight Clojure library for managing SQL migrations stored as EDN files and 
                 executing them against a relational database. The project provides a migration-runner protocol and a MySQL implementation using next.jdbc."
   :url "https://github.com/josemorista/eboshi"
-  :deploy-repositories {"github" {:url "https://maven.pkg.github.com/josemorista/eboshi"
-                                  :username :env/GITHUB_ACTOR
-                                  :password :env/GITHUB_TOKEN
-                                  :sign-releases false}}
+  :deploy-repositories {"clojars" {:sign-releases false
+                                   :username :env/CLOJARS_USERNAME
+                                   :password :env/CLOJARS_TOKEN}}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
@@ -16,6 +15,7 @@
                                   [org.testcontainers/mysql "1.21.3"]]
                    :source-paths ["src" "test"]}
              :uberjar {:aot [:all]}}
-  :aliases {"test" ["with-profile" "+dev" "test"]}
+  :aliases {"test" ["with-profile" "+dev" "test"]
+            "deploy:clojars" ["with-profile" "+uberjar" "deploy" "clojars"]}
   :main eboshi.core
   :repl-options {:init-ns eboshi.core})
