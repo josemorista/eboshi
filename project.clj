5276b7c4fffa7e9dc025a44a7e1f2ee59928c785
josemorista <josemorista@gmail.com>
2025-11-21 11:37:47 -0300

    ci: publish to clojars

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
                 executing them against a relational database. The project provides a migration-runner protocol and a MySQL implementation using next.jdbc."
   :url "https://github.com/josemorista/eboshi"
   :deploy-repositories {"clojars" {:username :env/CLOJARS_USERNAME
-                                   :password :env/CLOJARS_TOKEN}}
+                                   :password :env/CLOJARS_TOKEN
+                                   :sign-releases false}}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
