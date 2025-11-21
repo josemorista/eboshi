3e867fe43269880d2dc39fa8f96ee12f317a60d7
josemorista <josemorista@gmail.com>
2025-11-21 11:23:37 -0300

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.josemorista/eboshi "0.3.0-SNAPSHOT"
+(defproject org.clojars.josemorista/eboshi "0.3.1-SNAPSHOT"
   :description "Eboshi is a lightweight Clojure library for managing SQL migrations stored as EDN files and 
                 executing them against a relational database. The project provides a migration-runner protocol and a MySQL implementation using next.jdbc."
   :url "https://github.com/josemorista/eboshi"
